d5865ca110ea5528adb1c403f4f2da45330782fe
Tom Besio <thomas.besio@gmail.com>
2014-11-28 10:17:48 -0600

    Added Validation for Pantry Item

--- app/models/pantry_item.rb
+++ app/models/pantry_item.rb
@@ -3,4 +3,10 @@ class PantryItem < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
 
+  validates :user_id, :presence => true
+  validates :item_id, :presence => true
+  validates :purchase_date, :presence => true
+  validates :consumption_speed, :presence => true
+  validates :recurring, :presence => true
+
 end
